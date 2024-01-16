d12e52be2dee9e7d56985c9a47f7cda9806285df
reiji7 <ld3576@gmail.com>
2024-01-16 23:00:02 +0900

    Share app feature & Show invitedBy

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,16 @@ import WebApp from "@twa-dev/sdk";
 import { TonConnectButton } from "@tonconnect/ui-react";
 import { useCounterContract } from "./hooks/useCounterContract";
 
+const shareURI = "https://t.me/share/url?url=";
+const appURI = "t.me/PlayLuckyBot/demo";
+
 function App() {
   const { value, address } = useCounterContract();
   // https://core.telegram.org/bots/webapps#webappinitdata
   const initData = WebApp?.initDataUnsafe;
   const tgId = initData?.user?.id;
+  const tgUserName = initData?.user?.username;
+  const invitedBy = initData?.start_param;
 
   return (
     <div className="App">
@@ -15,12 +20,32 @@ function App() {
         <div className="Card">
           <b>Telegram ID</b>
           <div>{tgId}</div>
+          <b>Username</b>
+          <div>{tgUserName}</div>
         </div>
-        <div className="Card">
-          <b>Invited by</b>
-          <div>{"Loading..."}</div>
+        {invitedBy && (
+          <div className="Card">
+            <b>Invited by</b>
+            <div>{invitedBy}</div>
+          </div>
+        )}
+        <div
+          className="Button"
+          onClick={() => {
+            // startapp as payload for direct link mini apps
+            // https://core.telegram.org/bots/webapps#direct-link-mini-apps
+            const referralURI = tgUserName
+              ? `${appURI}?startapp=${tgUserName}`
+              : appURI;
+            // share feature: https://core.telegram.org/widgets/share
+            // From SDK 7.0, openTelegramLink no longer closes the mini app, which enables in-app sharing
+            WebApp.openTelegramLink(
+              shareURI.concat(referralURI, "&text=").concat("Share testing...")
+            );
+          }}
+        >
+          Share
         </div>
-        <div className="Button">Share</div>
       </div>
       <div className="Container">
         <TonConnectButton />
